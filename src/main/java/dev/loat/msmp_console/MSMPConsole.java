680d6be82c5537959bc171bd222bb9de7c447b36
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-03-21 14:21:47 +0100

    init

---
+++ src/main/java/dev/loat/msmp_console/MSMPConsole.java
@@ -0,0 +1,10 @@
+package dev.loat.msmp_console;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MSMPConsole implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
